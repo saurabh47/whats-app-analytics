f2f7985aa5060f3696a26bb4908b373abbc148f5
Saurabh Gangamwar <saurabhgangamwar78@gmail.com>
2022-02-06 09:21:06 +0000

    Parameterized AWS Credentails

--- src/@common/constant/config.ts
+++ src/@common/constant/config.ts
@@ -1,12 +1,12 @@
 export const COLOR_CODES = ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
 '#FF9655', '#FFF263', '#6AF9C4'];
 
-export const AWS_KEY = 'AKIASGZBPD5QZCAPUZ3O';
+export const AWS_KEY = '{{AWS.ACCESS_KEY_ID}}';
 
-export const AWS_SECRET = 'kY0Usx8nHZprH5qJ1z9t4zjuuR8aEQGXmYta0m6q';
+export const AWS_SECRET = '{{AWS.SECRET_ACCESS_KEY}}';
 
-export const AWS_S3_BUCKET = 'whatsapp-analytics-users-data'
+export const AWS_S3_BUCKET = '{{AWS.S3_BUCKET_NAME}}'
 
-export const AWS_S3_BUCKET_DIRECTORY = 'user-data';
+export const AWS_S3_BUCKET_DIRECTORY = '{{AWS.S3_BUCKET_DIRECTORY}}';
 
 export const DEMO_APP_URL = 'http://whats-app-analytics-v2.s3-website.ap-south-1.amazonaws.com/';
